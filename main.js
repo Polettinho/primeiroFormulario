f825ea6f142fdc742653a843073f346aa5de3f88
Eduardo Poletto <eduardopoletto68@gmail.com>
2021-11-14 12:34:59 -0400

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// JavaScript
+
+function armazenarDados(){
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;    
+}
